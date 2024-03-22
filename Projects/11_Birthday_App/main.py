3d6cbebc502d4323c7c78d9f9a6d61f8b28bf4eb
ppalmeros <pedrofpalmeros@gmail.com>
2024-03-21 22:41:18 -0600

    [ADD]   Birthday app

--- Projects/11_Birthday_App/main.py
+++ Projects/11_Birthday_App/main.py
@@ -1,23 +1,53 @@
+import pandas as pd
+from datetime import datetime
+
+import random
 import smtplib
-from email.message import EmailMessage
+##################### Hard Starting Project ######################
+
+# 1. Update the birthdays.csv with your friends & family's details. 
+# HINT: Make sure one of the entries matches today's date for testing purposes. 
+
+# 2. Check if today matches a birthday in the birthdays.csv
+# HINT 1: Only the month and day matter. 
+# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
+# birthdays_dict = {
+#     (month, day): data_row
+# }
+#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
+# if (today_month, today_day) in birthdays_dict:
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pd.read_csv("birthdays.csv")
 
 
-my_email = "palmeros.test@gmail.com"
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-try:
-    with open("password.txt","r") as passfile:
-        password = passfile.readline()
-except FileNotFoundError:
-        print("ERROR NO PASSWORD FILE")
-        password = "NOT FOUND"
-else: 
-    print(password)
-
-mail_message = """Subject:Hello\n\nThis is the body of my email"""
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f".\\letter_templates\\letter_{random.randint(1,3)}.txt"
+    with open (file_path) as letter_file: 
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("Angela", "Pedro Flores")
 
 
-with smtplib.SMTP('smtp.gmail.com:587') as connection:
-    connection.starttls()
-    connection.login(user=my_email, password = password)
-    connection.sendmail(from_addr=my_email, to_addrs="pedrofpalmeros@gmail.com", msg=mail_message)
+    with smtplib.SMTP("smtp.gmail.com",587) as connection: 
+        with open(".\\password.txt") as passfile: 
+            my_mot_pass = passfile.readline().rstrip()
+        connection.starttls()
+        connection.login("palmeros.test@gmail.com",my_mot_pass)
+        mail_content = f"Subject:Happy Birthday!\n\n{contents}"
+        connection.sendmail(
+            from_addr = "pedrofpalmeros@gmail.com",
+            to_addrs= birthday_person["email"], msg=mail_content
+        )
 
