4fe8723879a79e01787977ed12059a3615f54759
ppalmeros <pedrofpalmeros@gmail.com>
2024-03-23 14:48:03 -0600

    [ADD] Open Trivia project

--- Projects/13_Open_Trivia/quiz_brain.py
+++ Projects/13_Open_Trivia/quiz_brain.py
@@ -16,15 +16,16 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
-        return f"Q.{self.question_number}: {q_text} (True/False):"
+        return f"Q.{self.question_number}: {q_text}:"
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
+
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
+            return False 
+    def get_score(self):
+        return self.score
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
